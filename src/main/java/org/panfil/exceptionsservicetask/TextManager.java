061c42086ef85f18b82ffc264de1ecbcef1e9ef7
EPanfil <panfilelena82@gmail.com>
2023-05-31 16:21:42 +0300

    Added the implementation of Checked and Unchecked exceptions.

---
+++ src/main/java/org/panfil/exceptionsservicetask/TextManager.java
@@ -0,0 +1,33 @@
+package org.panfil.exceptionsservicetask;
+
+public class TextManager {
+    public static int getTheTextLength(String inputString) {
+        int stringLength = inputString.length();
+        return stringLength;
+    }
+
+    public static int getTheTextLengthWithTryCatchFinally(String inputString) {
+        try {
+            return inputString.length();
+
+        } catch (NullPointerException capturedExceptionInTryBlock) {
+            System.out.println("An exception has been captured with the following message: " + capturedExceptionInTryBlock.getMessage());
+            return 0;
+
+        } finally {
+            System.out.println("This block is executed no matter what ");
+        }
+
+    }
+
+    public static int getTheTextLengthIfElse(String inputString) {
+        if (inputString == null) {
+            System.out.println("Input string is a null value, be careful! ");
+            return 0;
+
+        } else {
+            return inputString.length();
+        }
+    }
+
+}
